2e3c5b5826c725a31a2565f1835a9ede3cc11668
표호근 <pyohogeun@gmail.com>
2019-08-18 10:32:35 +0900

    deploy 성공 - firebase init할때 꼭 build폴더 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
