55224953f411f30527b60a19bc0979d4a70df4ce
pyo <pyohogeun@gmail.com>
2019-08-26 01:10:16 +0900

    datepicker 한글버전 적용

--- src/components/timestampcustom.js
+++ src/components/timestampcustom.js
@@ -10,25 +10,69 @@ import {
 import SaveIcon from '@material-ui/icons/Save';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import koLocale from "date-fns/locale/ko";
 
 
-export default function MaterialUIPickers() {
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+const styles = (theme) => ({
+  root: {
+    backgroundColor: 'red',
+  }
+});
 
-  function handleDateChange(date) {
-    setSelectedDate(date);
+class MaterialUIPickers extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      selectedDate: new Date(),
+      setSelectedDate: new Date()
+    }
+    this.handleDateChange = this.handleDateChange.bind(this);
   }
 
-  return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container justify="space-around">
-        <Paper>
-          <DateTimePicker value={selectedDate} onChange={handleDateChange} />
-          <Button>
-            <SaveIcon></SaveIcon>
-          </Button>
-        </Paper>
-      </Grid>
-    </MuiPickersUtilsProvider>
-  );
+  handleDateChange(date) {
+    this.setState({
+      setSelectedDate: date 
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return(
+      <div>
+        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={koLocale}>
+          <Grid container justify="space-around">
+            <Paper>
+              <DateTimePicker value={this.state.selectedDate} onChange={this.state.handleDateChange} />
+              <Button>
+                <SaveIcon></SaveIcon>
+              </Button>
+            </Paper>
+          </Grid>
+        </MuiPickersUtilsProvider>
+      </div>
+    )
+  }
 }
+
+export default withStyles(styles)(MaterialUIPickers);
+// export default function MaterialUIPickers() {
+//   const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+//   function handleDateChange(date) {
+//     setSelectedDate(date);
+//   }
+
+//   return (
+//     <MuiPickersUtilsProvider utils={DateFnsUtils}>
+//       <Grid container justify="space-around">
+//         <Paper>
+//           <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+//           <Button>
+//             <SaveIcon></SaveIcon>
+//           </Button>
+//         </Paper>
+//       </Grid>
+//     </MuiPickersUtilsProvider>
+//   );
+// }
